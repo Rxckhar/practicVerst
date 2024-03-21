2db4fd459a72b71f6027a590f438d3c8a1d268c8
Safin Ruslan <72904245+Rxckhar@users.noreply.github.com>
2024-03-22 01:02:41 +0500

    Add files via upload

---
+++ PracticVerst/scripts/script.js
@@ -0,0 +1,6 @@
+document.getElementById('menu_button').addEventListener('click', function() {
+    document.getElementById('menu').style.display = 'block';
+  });
+document.getElementById('menu_svg').addEventListener('click', function() {
+    document.getElementById('menu').style.display = 'none';
+  });
